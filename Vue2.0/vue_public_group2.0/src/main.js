2ec43dd8102b263c243b42423e5ed2734ef5ffe1
wzg15801407309 <1051084301@qq.com>
2023-11-19 17:53:32 +0800

    vue2.0ui框架

--- Vue2.0/vue_public_group2.0/src/main.js
+++ Vue2.0/vue_public_group2.0/src/main.js
@@ -1,5 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+// 全部引入的方式 按需引用方式用在做项目中比较方便
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
